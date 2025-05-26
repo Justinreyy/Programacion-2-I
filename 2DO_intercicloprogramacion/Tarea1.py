c36da8471db291bd589e0475e1fd9dea0c7f7219
Justinreyy <reyjustin681@gmail.com>
2025-05-26 12:08:46 -0500

    Bloque 3

---
+++ 2DO_intercicloprogramacion/Tarea1.py
@@ -0,0 +1,10 @@
+x= 5
+print("Hola")
+def print_lyrics() :
+    print("soy un leñador, y estoy bien")
+    print("duermo toda la noche y trabajo todo el dia")
+print("yo")
+x= x + 2
+print_lyrics()
+print(x)
+
